5f1a2fc668712dd9f7ab6eb5c8f05340fa685d0c
Taha <tahabozoyuk@gmail.com>
2023-12-15 17:08:09 +0300

    CLI

---
+++ script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+dnf update -y
+dnf install git -y
+dnf install python3 -y
+dnf install python3-pip -y
+pip3 install flask
+cd /home/ec2-user
+git clone https://github.com/tahabozoyuk/Roman-numerals-EC2-CFN_CLI.git
+cd Roman-numerals-EC2-CFN_CLI
+chmod +x roman-numerals-converter-app.py
+python3 roman-numerals-converter-app.py
